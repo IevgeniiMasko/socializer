e1c9d008206c5bb9c0f604118b0de844bd5d1a17
Ievgenii Masko <ievgenii.masko@gmail.com>
2022-10-18 23:19:17 +0200

    start11

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://socializer1.herokuapp.com";
 
 //General axios
 export default axios.create({ baseURL: BASE_URL, withCredentials: true });
